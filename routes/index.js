2707844287c7a50a7fc464a099a4b518e0aa8674
sandra.escalonamoreno@gmail.com <sandra.escalonamoreno@gmail.com>
2015-05-12 13:05:08 +0200

    Registro

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' , errors:[]});
@@ -10,6 +12,13 @@ router.get('/', function(req, res) {
 
 // Autoload de comando con :quizId
 router.param('quizId', quizController.load);
+
+//Difinicon de rutas de sesion
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
+
+
 
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
