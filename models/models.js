c3b8204069bb8c1cdeddb093ae0fef8f35a175b5
sandra.escalonamoreno@gmail.com <sandra.escalonamoreno@gmail.com>
2015-05-12 11:57:39 +0200

    Añadir comentario

--- models/models.js
+++ models/models.js
@@ -31,8 +31,15 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var quiz_pth = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-exports.Quiz = Quiz;
 
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+exports.Quiz = Quiz;
+exports.Comment = Comment;
 
 
 // sequelize.sync() crea las tablas de datos definidas en el modelo
