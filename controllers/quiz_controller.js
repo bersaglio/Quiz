b4d7d58a106667aafe1ab728ee6ca4b0892a71a9
sandra.escalonamoreno@gmail.com <sandra.escalonamoreno@gmail.com>
2015-05-12 10:25:31 +0200

    Tema12

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -8,16 +8,41 @@ exports.load = function(req, res, next, quizId){
       if(quiz){
         req.quiz = quiz;
         next();
-      } else { next new Error ('No existe quizId=' + quizId));}
+      } else { next (new Error ('No existe quizId=' + quizId));}
     }
-  }.catch(function(error){ next(error);});
+  ).catch(function(error){ next(error);});
+};
+
+//GET /quizes/new
+exports.new = function(req, res){
+ var quiz = models.Quiz.build(
+  {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+ res.render('quizes/new',{ quiz: quiz, errors: [] });
+};
+
+
+// POST /quizes/create
+exports.create=function(req, res){
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  quiz.validate().then(function(err){
+    if(err){
+      res.render('quizes/new', {quiz: quiz, errors: err.errors, title: 'Crear'});
+    }else{
+      quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+      res.redirect('/quizes');
+    }) 
+    }
+    });
 };
 
 
 //GET /quizes/:id
 exports.show = function(req, res){
   models.Quiz.find(req.params.quizId).then(function(quiz) {
-      res.render('quizes/show', { quiz: req.quiz });
+      res.render('quizes/show', { quiz: req.quiz, errors: [] });
     
   })
 };	
@@ -26,9 +51,9 @@ exports.show = function(req, res){
 exports.answer = function(req, res){
   models.Quiz.find(req.params.quizId).then(function(quiz) {
     if(req.query.respuesta === req.quiz.respuesta){
-    	res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Correcto' });	
+    	res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Correcto', errors: []  });	
     } else{
-    	res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Incorrecto' });	 	
+    	res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Incorrecto', errors: [] });	 	
     }
     
   })
@@ -38,12 +63,12 @@ exports.answer = function(req, res){
 //get /quizes
 exports.index = function(req, res){
     if(req.query.search){
-        models.Quiz.findAll({where: ["pregunta like ?", '%'+req.query.search+'%']}).then(function(quizes){
-            res.render('quizes', {quizes: quizes});
-        })
+        models.Quiz.findAll({where: ["pregunta like ?", '%'+req.query.search+'%'], order: 'pregunta ASC'})
+        .then(function(quizes){
+             res.render('quizes/index.ejs', {quizes: quizes, title: 'Listado', errors: [] })}).catch(function(error) { next(error);});
     }else{
     models.Quiz.findAll().then(function(quizes){
-        res.render('quizes', {quizes: quizes});
+        res.render('quizes', {quizes: quizes, errors:[]});
     }).catch(function(error){ next(error);})
 }
     
